448730d7b6c062a49e83616256cf818c5dedc295
Kirill32rus <Kirill32rus_666@mail.ru>
2021-05-02 14:15:31 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        final double STEPS_IN_SPAWN = 2;
+        final double SPANS_IN_FLOOR = 2;
+
+        double floor, liveInStep;
+        double result;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Эта программа рассчитывает сколько вы получите доп. секунд жизни, поднявшись на определённый этаж.");
+
+        System.out.print("Введите этаж, на который хотите взобраться:");
+        floor = input.nextDouble();
+
+        System.out.print("Сколько секунд даёт одна ступенка: ");
+        liveInStep = input.nextDouble();
+
+        result = liveInStep * STEPS_IN_SPAWN * SPANS_IN_FLOOR * floor;
+        System.out.println("Ваша жизнь увеличилась на: " + result + " сек");
+    }
+}
